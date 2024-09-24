e6e2738b59f1f79f055f363a16be4a69d10eb025
Aimsucks <contact@aimsucks.space>
2024-09-23 20:50:32 -0400

    feat: added header row

--- CodexExample/Windows/MainWindow.cs
+++ CodexExample/Windows/MainWindow.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Numerics;
-using Dalamud.Interface.Internal;
-using Dalamud.Interface.Utility;
+using Dalamud.Interface;
+using Dalamud.Interface.ManagedFontAtlas;
 using Dalamud.Interface.Windowing;
 using Dalamud.Plugin.Services;
 using ImGuiNET;
@@ -12,16 +12,21 @@ public class MainWindow : Window, IDisposable
 {
     private readonly string IconImagePath;
     private readonly Plugin Plugin;
-
-    // We give this window a hidden ID using ##
-    // So that the user will see "Codex Window" as window title,
-    // but for ImGui the ID is "Codex Window##With a hidden ID"
+    
+    private readonly int IconWidth = 100;
+    private readonly int HeaderPadding = 20;
+    
+    // Create a larger font for the header
+    private readonly IFontHandle HeaderFont = Plugin.PluginInterface.UiBuilder.FontAtlas.NewDelegateFontHandle(
+        e => e.OnPreBuild(
+            t => t.AddDalamudDefaultFont(20)));
+    
     public MainWindow(Plugin plugin, string iconImagePath)
         : base("Codex Example##With a hidden ID", ImGuiWindowFlags.NoScrollbar | ImGuiWindowFlags.NoScrollWithMouse)
     {
         SizeConstraints = new WindowSizeConstraints
         {
-            MinimumSize = new Vector2(375, 330),
+            MinimumSize = new Vector2(450, 300),
             MaximumSize = new Vector2(float.MaxValue, float.MaxValue)
         };
 
@@ -29,11 +34,48 @@ public class MainWindow : Window, IDisposable
         Plugin = plugin;
     }
 
-    public void Dispose() { }
+    public void Dispose()
+    {
+        HeaderFont.Dispose();
+    }
 
     public override void Draw()
     {
         ImGui.Text($"The random config bool is {Plugin.Configuration.SomePropertyToBeSavedAndWithADefault}");
+        ImGui.Separator();
+        
+        if (ImGui.BeginTable("headerTable", 2))
+        {
+            ImGui.TableSetupColumn("one", ImGuiTableColumnFlags.WidthFixed, IconWidth + HeaderPadding);
+            // ImGui.TableSetupColumn("two", ImGuiTableColumnFlags.WidthStretch);
+            
+            ImGui.TableNextRow();
+            
+            // Icon column
+            ImGui.TableNextColumn();
+            
+            ImGui.SetCursorPosX(ImGui.GetCursorPosX() + (HeaderPadding / 2f));
+            var iconImage = Plugin.TextureProvider.GetFromFile(IconImagePath).GetWrapOrDefault();
+            if (iconImage != null) ImGui.Image(iconImage.ImGuiHandle, new Vector2(IconWidth, IconWidth));
+
+            // Text column
+            ImGui.TableNextColumn();
+            
+            // Header text
+            if(HeaderFont.Available)
+            {
+                HeaderFont.Push();
+                ImGui.Text("Codex Example");
+                HeaderFont.Pop();
+            } else ImGui.Text("Codex Example");
+            
+            // Description text
+            ImGui.TextWrapped("Here you will find an example of how a plugin can interact with the Codex plugin preset API. Please review the two tabs below to understand how the plugin functions.");
+
+            ImGui.EndTable();
+        }
+        
+        ImGui.Separator();
 
         if (ImGui.Button("Show Settings"))
         {
